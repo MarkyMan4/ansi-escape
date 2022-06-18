922f47d59341726565515e0bb68930825a88cc94
MarkyMan4 <markus.hutnik@gmail.com>
2022-06-18 08:54:54 -0500

    single char input without pressing enter

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
 import os
-from tkinter import W
+import sys
+import termios
+import tty
 
 class Game:
-    def __init__(self):
-        self.board = [['.' for _ in range(20)] for _ in range(10)]
+    def __init__(self, board_rows, board_cols):
+        self.board = [['.' for _ in range(board_rows)] for _ in range(board_cols)]
         self.player_y = int(len(self.board) / 2)
         self.player_x = int(len(self.board[0]) / 2)
         self.board[self.player_y][self.player_x] = '+'
@@ -25,23 +27,39 @@ class Game:
         self.update_board()
 
     def move_up(self):
-        self.update_player_pos(self.player_x, self.player_y - 1)
+        if self.player_y > 0:
+            self.update_player_pos(self.player_x, self.player_y - 1)
 
     def move_left(self):
-        self.update_player_pos(self.player_x - 1, self.player_y)
+        if self.player_x > 0:
+            self.update_player_pos(self.player_x - 1, self.player_y)
 
     def move_down(self):
-        self.update_player_pos(self.player_x, self.player_y + 1)
+        if self.player_y < len(self.board) - 1:
+            self.update_player_pos(self.player_x, self.player_y + 1)
 
     def move_right(self):
-        self.update_player_pos(self.player_x + 1, self.player_y)
+        if self.player_x < len(self.board[0]) - 1:
+            self.update_player_pos(self.player_x + 1, self.player_y)
+
+def get_char():
+    """
+    Get a single character from stdin without the user having to press enter
+    """
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    tty.setraw(sys.stdin.fileno())
+    ch = sys.stdin.read(1)
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return ch
 
 def main():
-    game = Game()
+    game = Game(20, 10)
     game.update_board()
 
     while True:
-        inp = input()
+        inp = get_char()
 
         if inp == 'w':
             game.move_up()
@@ -51,5 +69,7 @@ def main():
             game.move_down()
         elif inp == 'd':
             game.move_right()
+        elif inp == 'q':
+            sys.exit()
 
 if __name__ == '__main__': main()
